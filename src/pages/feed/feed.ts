7caeecc45ebae4cb8cb15234b6e894acf794c985
Isabel Rodrigues <isabelrodrgs@gmail.com>
2018-11-24 15:56:59 -0200

    slides, home e feed

---
+++ src/pages/feed/feed.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the FeedPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-feed',
+  templateUrl: 'feed.html',
+})
+export class FeedPage {
+
+  public nomeUsuario = "Bell Rodrigues";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad FeedPage');
+  }
+
+}
